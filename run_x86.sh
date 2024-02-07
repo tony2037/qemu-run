5f9a4ee653f8b6395933c4944ae816be2d66d320
ztex <ztex030640417@gmail.com>
2024-02-06 18:57:11 -0800

    Fix: Add rw to make rootfs writable

--- run_x86.sh
+++ run_x86.sh
@@ -1,5 +1,5 @@
 qemu-system-x86_64 -machine q35\
     -kernel Image \
     -drive file=rootfs.ext4,if=virtio,format=raw \
-    -append 'root=/dev/vda console=ttyS0' -vga none -display none \
+    -append 'root=/dev/vda rw console=ttyS0' -vga none -display none \
     -serial mon:stdio
