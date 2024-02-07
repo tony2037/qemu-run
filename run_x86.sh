6b6527ee4628f0148d0cc24cee5ad6ba03ac7779
ztex <ztex030640417@gmail.com>
2024-02-06 18:44:08 -0800

    Add: script for run x86_64

---
+++ run_x86.sh
@@ -0,0 +1,5 @@
+qemu-system-x86_64 -machine q35\
+    -kernel Image \
+    -drive file=rootfs.ext4,if=virtio,format=raw \
+    -append 'root=/dev/vda console=ttyS0' -vga none -display none \
+    -serial mon:stdio
